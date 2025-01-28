d9c7aff2950c2e9e025efba41d5c11c452f97280
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2025-01-28 16:15:09 +0300

    task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,51 +1,33 @@
 #!/usr/bin/env python3
+"""First-In First-Out caching module.
+"""
+from collections import OrderedDict
 
-'''Task 1: FIFO caching
-'''
-
-
-from typing import Any, Optional
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    '''A class `FIFOCache` that inherits from
-       `BaseCaching` and is a caching system.
-    '''
-
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a FIFO
+    removal mechanism when the limit is reached.
+    """
     def __init__(self):
+        """Initializes the cache.
+        """
         super().__init__()
+        self.cache_data = OrderedDict()
 
-    def put(self, key: Any, item: Any) -> None:
-        '''put method for adding data to cache
-        '''
-        if key is not None and item is not None:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                cached_data = list(self.cache_data.keys())
-                first_key = cached_data[0]
+    def put(self, key, item):
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
 
-                self.cache_data.pop(first_key)
-
-                print("DISCARD: {}".formart(first_key))
-
-                del first_key
-            self.cache_data[key] = item
-
-    def get(self, key: Any) -> Optional[Any]:
-        '''get method for reteiving cache
-        '''
-        return self.cache_data.get(key)
-
-if __name__ == "__main__":
-    my_cache = FIFOCache()
-    my_cache.put("A", "Hello")
-    my_cache.put("B", "World")
-    my_cache.put("C", "ALX")
-    my_cache.put("D", "School")
-    my_cache.print_cache()
-    my_cache.put("E", "Battery")
-    my_cache.print_cache()
-    my_cache.put("C", "Street")
-    my_cache.print_cache()
-    my_cache.put("F", "Mission")
-    my_cache.print_cache()
+    def get(self, key):
+        """Retrieves an item by key.
+        """
+        return self.cache_data.get(key, None)
